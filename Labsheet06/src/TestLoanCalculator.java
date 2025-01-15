2b3ea338adf37e792447426387268bc37b222a40
NattaponYn <nattapon8129@gmail.com>
2025-01-15 11:11:52 +0700

    Labsheet06

---
+++ Labsheet06/src/TestLoanCalculator.java
@@ -0,0 +1,12 @@
+public class TestLoanCalculator {
+ 
+	public static void main(String[] args) {
+		LoanCalculator ld1 = new LoanCalculator();
+		
+		ld1.setLoanDetails("Labtop", 50000, 5, 2);
+		
+		ld1.displayLoanDetails();
+ 
+	}
+ 
+}
